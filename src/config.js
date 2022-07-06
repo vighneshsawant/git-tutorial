da01d0701a7eebb6ccc39ade4a7bbb12530182ab
vighnesh <vsawant10@gmail.com>
2022-07-05 20:35:26 -0400

    version 4 -- donwloaded git

--- src/config.js
+++ src/config.js
@@ -12,3 +12,9 @@ console.log('version 2 -- 07052022')
 updated by : vighnesh on july 5, 2022
 */
 console.log('version 3 -- 07052022')
+
+/*
+updated by : vighnesh on july 5, 2022
+this code version belongs to downloaded/cloned git
+*/
+console.log('version 4 -- 07052022')
--- src/index.js
+++ src/index.js
@@ -7,3 +7,9 @@ console.log('version 1 -- 07052022')
 updated by : vighnesh on july 5, 2022
 */
 console.log('version 3 -- 07052022')
+
+/*
+updated by : vighnesh on july 5, 2022
+this code version belongs to downloaded/cloned git
+*/
+console.log('version 4 -- 07052022')
