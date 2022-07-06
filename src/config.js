26d8b83789ba3b92ac3ab1091bcee75846413246
vighnesh <vsawant10@gmail.com>
2022-07-05 20:21:49 -0400

    version 3 on july 5, 2022

--- src/config.js
+++ src/config.js
@@ -7,3 +7,8 @@ console.log('version 1 -- 07052022')
 updated by : vighnesh on july 5, 2022
 */
 console.log('version 2 -- 07052022')
+
+/*
+updated by : vighnesh on july 5, 2022
+*/
+console.log('version 3 -- 07052022')
--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@
 added by : vighnesh on july 5, 2022
 */
 console.log('version 1 -- 07052022')
+
+/*
+updated by : vighnesh on july 5, 2022
+*/
+console.log('version 3 -- 07052022')
